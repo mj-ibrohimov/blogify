c159f733b2062881f95b0c7988d1ffabb64926b4
Muhammadjon_Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-11-05 14:49:56 +0100

    Replace old source code with new source code

--- website/website/settings.py
+++ website/website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c7k(2^f%vzp*u1=w@2w+d6u$sz(l=^3c2_4b2gwn1#5b*c4ze1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["BlogifyDjango.pythonanywhere.com", '127.0.0.1', 'localhost']
 
 
 # Application definition
